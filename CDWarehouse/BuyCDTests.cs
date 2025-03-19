c65a84910294e812fc2f6bd712f4fda06e5b3703
Tanzeel Ahmed <tanzeel.ahmed@tba.group>
2025-03-19 13:18:17 +0000

    Moved warehouse instantiation to test setup

--- CDWarehouse/BuyCDTests.cs
+++ CDWarehouse/BuyCDTests.cs
@@ -7,10 +7,12 @@ namespace CDWarehouse
     public class BuyCDTests
     {
 
-        public CreditCard _card;
-        public Mock<IChartNotifier> _notifyChart;
-        public Mock<ICompetitorPrices> _competitorPrices;
-        public Mock<ITop100Chart> _top100Chart;
+        private CreditCard _card;
+        private Mock<IChartNotifier> _notifyChart;
+        private Mock<ICompetitorPrices> _competitorPrices;
+        private Mock<ITop100Chart> _top100Chart;
+        private WareHouse _wareHouse;
+
 
         [SetUp]
         public void Setup()
@@ -19,15 +21,16 @@ namespace CDWarehouse
             _notifyChart = new Mock<IChartNotifier>();
             _competitorPrices = new Mock<ICompetitorPrices>();
             _top100Chart = new Mock<ITop100Chart>();
+            _wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
         }
 
         [Test]
         public void BuyCdInStockReducesQuantity()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1 };
             Customer customer = new Customer();
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
 
             NUnit.Framework.Assert.That(cd.Quantity, Is.EqualTo(0));
         }
@@ -35,31 +38,31 @@ namespace CDWarehouse
         [Test]
         public void BuyCdOutOfStockDoesntReduceQuantity()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 0 };
             Customer customer = new Customer();
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
             NUnit.Framework.Assert.That(cd.Quantity, Is.EqualTo(0));
         }
 
         [Test]
         public void BuyCdInStockAssignsUpdatesCustomerQuantity()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1 };
             Customer customer = new Customer();
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
             NUnit.Framework.Assert.That(customer.cdTitles.Count, Is.EqualTo(1));
         }
 
         [Test]
         public void BuyCDInStockCreditCardRejected()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1 };
             Customer customer = new Customer();
             _card.IsValid = false;
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
             NUnit.Framework.Assert.That(customer.cdTitles.Count, Is.EqualTo(0));
             NUnit.Framework.Assert.That(cd.Quantity, Is.EqualTo(1));
         }
@@ -67,10 +70,10 @@ namespace CDWarehouse
         [Test]
         public void BuyCDInStockCreditCardAccepted()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1 };
             Customer customer = new Customer();
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
             NUnit.Framework.Assert.That(customer.cdTitles.Count, Is.EqualTo(1));
             NUnit.Framework.Assert.That(cd.Quantity, Is.EqualTo(0));
         }
@@ -78,24 +81,24 @@ namespace CDWarehouse
         [Test]
         public void BuyCdNotifyCharts()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1 };
             Customer customer = new Customer();
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
             _notifyChart.Verify(x => x.Notify("Tanzeel", "Greatest Hits", 1));
         }
 
         [Test]
         public void BuyCdInTop100()
         {
-            var wareHouse = new WareHouse(_notifyChart.Object, _top100Chart.Object, _competitorPrices.Object);
+
             Customer customer = new Customer();
             CD cd = new CD { Title = "Tanzeel's Greatest Hits", Quantity = 1, Price = 10.0 };
 
             _top100Chart.Setup(x => x.IsTop100(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
             _competitorPrices.Setup(x => x.GetLowestPrice(It.IsAny<string>(), It.IsAny<string>())).Returns(9.0);
 
-            wareHouse.BuyCd(cd, customer, _card);
+            _wareHouse.BuyCd(cd, customer, _card);
 
             NUnit.Framework.Assert.That(cd.Price, Is.EqualTo(8.0));
         }
